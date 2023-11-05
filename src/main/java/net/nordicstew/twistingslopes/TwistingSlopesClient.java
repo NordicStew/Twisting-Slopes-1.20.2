754ab5d9e745f5258f9dce65a15fa0da19504ac5
Jacob Gahlon <j.gahlon@outlook.com>
2023-11-05 16:03:45 -0600

    Initial commit

---
+++ src/main/java/net/nordicstew/twistingslopes/TwistingSlopesClient.java
@@ -0,0 +1,10 @@
+package net.nordicstew.twistingslopes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TwistingSlopesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
